970be75070937a6c9cd06d3fa8bd62c2b57f2eab
Eng.Ahmed ALAmoudi <76726399+AhmedAlamoudi7@users.noreply.github.com>
2021-11-08 04:29:25 +0200

    Add project files.

---
+++ Movies.Client/ApiServices/IMoviesApiService.cs
@@ -0,0 +1,18 @@
+﻿using Movies.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMoviesApiService
+    {
+
+        Task<IEnumerable<Movie>> GetMovies();
+        //Task<Movie> GetMovie(string id);
+        //Task<Movie> CreateMovie(Movie movie);
+        //Task<Movie> UpdateMovie(Movie movie);
+        //Task DeleteMovie(int id);
+    }
+}
---
+++ Movies.Client/ApiServices/MoviesApiService.cs
@@ -0,0 +1,118 @@
+﻿using Movies.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MoviesApiService : IMoviesApiService
+    {
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+
+            var listMovies = new List<Movie>();
+            listMovies.Add(
+
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Action",
+                    Title = "Thour",
+                    Rating = "5",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "AhmedR. Alamoudi"
+                }
+
+
+                );
+            return await Task.FromResult(listMovies);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ////////////////////////// //////////////////////// ////////////////////////
+            //// WAY 2 :
+
+            //// 1. "retrieve" our api credentials. This must be registered on Identity Server!
+            //var apiClientCredentials = new ClientCredentialsTokenRequest
+            //{
+            //    Address = "https://localhost:5005/connect/token",
+
+            //    ClientId = "movieClient",
+            //    ClientSecret = "secret",
+
+            //    // This is the scope our Protected API requires. 
+            //    Scope = "movieAPI"
+            //};
+
+            //// creates a new HttpClient to talk to our IdentityServer (localhost:5005)
+            //var client = new HttpClient();
+
+            //// just checks if we can reach the Discovery document. Not 100% needed but..
+            //var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            //if (disco.IsError)
+            //{
+            //    return null; // throw 500 error
+            //}
+
+            //// 2. Authenticates and get an access token from Identity Server
+            //var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);            
+            //if (tokenResponse.IsError)
+            //{
+            //    return null;
+            //}
+
+            //// Another HttpClient for talking now with our Protected API
+            //var apiClient = new HttpClient();
+
+            //// 3. Set the access_token in the request Authorization: Bearer <token>
+            //client.SetBearerToken(tokenResponse.AccessToken);
+
+            //// 4. Send a request to our Protected API
+            //var response = await client.GetAsync("https://localhost:5001/api/movies");
+            //response.EnsureSuccessStatusCode();
+
+            //var content = await response.Content.ReadAsStringAsync();
+
+            //var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            //return movieList;
+
+
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
